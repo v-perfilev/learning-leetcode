707bfeaa76a8cc6dd5a7adba21fce19450f978ea
Vladimir Perfilev <persoff68@gmail.com>
2023-11-05 12:00:00 +0100

    solve problem №70

---
+++ src/main/kotlin/problems/t70/Solution.kt
@@ -0,0 +1,15 @@
+package problems.t70
+
+class Solution {
+    fun climbStairs(n: Int): Int {
+        val cache = IntArray(n + 1)
+        cache[0] = 0
+        cache[1] = 1
+        cache[2] = 2
+
+        for (i in 3..n) {
+            cache[i] = cache[i - 1] + cache[i - 2]
+        }
+        return cache[n]
+    }
+}

5d770fdef6fb6fd8882da83b8ebb55d0a81195aa
Vladimir Perfilev <persoff68@gmail.com>
2023-11-11 12:00:00 +0100

    solve problem №217

---
+++ src/main/kotlin/problems/t217/Solution.kt
@@ -0,0 +1,12 @@
+package problems.t217
+
+class Solution {
+    fun containsDuplicate(nums: IntArray): Boolean {
+        val set = mutableSetOf<Int>()
+        for (num in nums) {
+            if (set.contains(num)) return true
+            set.add(num)
+        }
+        return false
+    }
+}

2c7aaac97d716543157efd32afc9beb620416820
Vladimir Perfilev <persoff68@gmail.com>
2024-01-08 09:12:36 +0100

    solve problem №938

---
+++ src/main/kotlin/problems/t938/Solution.kt
@@ -0,0 +1,17 @@
+package problems.t938
+
+class Solution {
+    fun rangeSumBST(root: TreeNode?, low: Int, high: Int): Int {
+        if (root == null) return 0
+        var sum = 0
+        if (root.`val` in low..high) sum += root.`val`
+        if (root.`val` > low) sum += rangeSumBST(root.left, low, high)
+        if (root.`val` < high) sum += rangeSumBST(root.right, low, high)
+        return sum
+    }
+}
+
+class TreeNode(var `val`: Int) {
+    var left: TreeNode? = null
+    var right: TreeNode? = null
+}

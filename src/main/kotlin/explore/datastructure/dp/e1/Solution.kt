56a61e690dc8a102ce3ba0de5526172fad0d9573
Vladimir Perfilev <persoff68@gmail.com>
2024-01-18 11:23:14 +0100

    solve problem №70

--- src/main/kotlin/explore/datastructure/dp/e1/Solution.kt
+++ src/main/kotlin/explore/datastructure/dp/e1/Solution.kt
@@ -2,11 +2,14 @@ package explore.datastructure.dp.e1
 
 class Solution {
     fun climbStairs(n: Int): Int {
-        val cache = IntArray(n + 2)
-        cache[0] = 0
-        cache[1] = 1
-        cache[2] = 2
-        for (i in 3..n) cache[i] = cache[i - 1] + cache[i - 2]
-        return cache[n]
+        if (n <= 1) return 1
+        var perultimate = 1
+        var previous = 2
+        for (i in 3..n) {
+            val tmp = previous
+            previous += perultimate
+            perultimate = tmp
+        }
+        return previous
     }
 }
--- src/main/kotlin/problems/t70/Solution.kt
+++ src/main/kotlin/problems/t70/Solution.kt
@@ -2,11 +2,11 @@ package problems.t70
 
 class Solution {
     fun climbStairs(n: Int): Int {
+        if (n <= 1) return 1
         val cache = IntArray(n + 1)
         cache[0] = 0
         cache[1] = 1
         cache[2] = 2
-
         for (i in 3..n) {
             cache[i] = cache[i - 1] + cache[i - 2]
         }

b90aa748c1908daf51f04e5cbffc528b31c8a301
Vladimir Perfilev <persoff68@gmail.com>
2024-03-15 09:53:02 +0100

    solve problem №930

--- src/main/kotlin/problems/t930/Solution.kt
+++ src/main/kotlin/problems/t930/Solution.kt
@@ -1,4 +1,17 @@
 package problems.t930
 
 class Solution {
+    fun numSubarraysWithSum(nums: IntArray, goal: Int): Int {
+        val count = mutableMapOf(0 to 1)
+        var sum = 0
+        var result = 0
+
+        for (num in nums) {
+            sum += num
+            result += count.getOrDefault(sum - goal, 0)
+            count[sum] = count.getOrDefault(sum, 0) + 1
+        }
+
+        return result
+    }
 }
